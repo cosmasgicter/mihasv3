a0ba270aad2d22124b01b09393486194c4fde015
Cosmas Kanchepa <alexisstar8@gmail.com>
2025-09-25 06:57:19 +0200

    fix: allow auth register CORS authorization header

--- netlify/functions/auth/register.js
+++ netlify/functions/auth/register.js
@@ -6,7 +6,10 @@ import { validateTurnstileToken } from '../_lib/turnstileValidator.js'
 async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, authorization, x-requested-with'
+  )
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
